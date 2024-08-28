c52f68cd43d1219c7b3a6f011bb81ade163f315b
kr1571an0 <alikajk27@gmail.com>
2024-08-28 15:52:49 +0000

    c

--- Rfile1.r
+++ Rfile1.r
@@ -4,14 +4,3 @@ data(nutrimouse)
 
 View(nutrimouse)
 
-# Save the 'gene' component to a CSV file
-write.csv(nutrimouse$gene, file = "nutrimouse_gene.csv", row.names = FALSE)
-
-# Save the 'lipid' component to a CSV file
-write.csv(nutrimouse$lipid, file = "nutrimouse_lipid.csv", row.names = FALSE)
-
-# Save the 'diet' component to a CSV file
-write.csv(nutrimouse$diet, file = "nutrimouse_diet.csv", row.names = FALSE)
-
-# Save the 'genotype' component to a CSV file
-write.csv(nutrimouse$genotype, file = "nutrimouse_genotype.csv", row.names = FALSE)
